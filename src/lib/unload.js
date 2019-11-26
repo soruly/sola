0f434f2b7cba76a27aec935efee04e2663e85779
soruly <soruly@gmail.com>
2019-11-26 14:19:56 +0800

    Fix prettier format

--- src/lib/unload.js
+++ src/lib/unload.js
@@ -3,9 +3,9 @@ const fetch = require("node-fetch");
 const unload = (relativePath, SOLA_SOLR_URL, SOLA_SOLR_CORE) =>
   new Promise(async (resolve, reject) => {
     try {
-      const coreInfo = await fetch(`${SOLA_SOLR_URL}admin/cores?wt=json`).then(
-        res => res.json()
-      );
+      const coreInfo = await fetch(
+        `${SOLA_SOLR_URL}admin/cores?wt=json`
+      ).then(res => res.json());
       await Promise.all(
         Object.values(coreInfo.status)
           .filter(e => e.name.indexOf(`${SOLA_SOLR_CORE}_`) === 0)
--- src/status.js
+++ src/status.js
@@ -4,9 +4,11 @@ const { SOLA_SOLR_URL, SOLA_SOLR_CORE } = process.env;
 
 (async () => {
   console.log(`Reading core info from ${SOLA_SOLR_URL}`);
-  const status = (await fetch(
-    `${SOLA_SOLR_URL}admin/cores?indexInfo=true&wt=json`
-  ).then(res => res.json())).status;
+  const status = (
+    await fetch(
+      `${SOLA_SOLR_URL}admin/cores?indexInfo=true&wt=json`
+    ).then(res => res.json())
+  ).status;
 
   const cores = {};
   for (let id in status) {
